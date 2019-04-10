902decf3cce4a546ee89921a0a96d46e1d988f96
George Meshcherov <meshcherov.g@gmail.com>
2019-04-10 08:20:40 +0500

    add brain-even game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+import {
+  getName, greet, writeRuleOfGame, evenGame,
+} from '..';
+
+const rules = 'Answer "yes" if number even otherwise answer "no".';
+
+greet();
+
+writeRuleOfGame(rules);
+
+getName();
+
+evenGame();
