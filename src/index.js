c7b0bd3eb70ae5977ec0659dc11cce8320ac3089
George Meshcherov <meshcherov.g@gmail.com>
2019-04-10 08:22:33 +0500

    add writeRuleOfGame, random, evenGame

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => {
+export const greet = () => {
   console.log('Welcom to the Brain Games!');
+};
+
+export const getName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}! \n`);
+};
+
+export const writeRuleOfGame = (rules) => {
+  console.log(`${rules} \n`);
+};
+const random = () => {
+  const minNum = 1;
+  const maxNum = 100;
+  const randomNum = Math.floor(Math.random() * (maxNum - minNum) + 1);
+  return randomNum;
+};
+
+export const evenGame = () => {
+  const number = random();
+  const correctAnswer = number % 2 ? 'NO' : 'YES';
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ').toUpperCase();
+  if (answer !== correctAnswer) {
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+    console.log('Let\'s try again, !');
+  } else {
+    console.log('Correct!');
+    evenGame();
+  }
 };
