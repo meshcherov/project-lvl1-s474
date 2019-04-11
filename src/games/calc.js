567ed2f7c73da8915a23eff9e000425a10125079
George Meshcherov <meshcherov.g@gmail.com>
2019-04-11 11:49:05 +0500

    add map mathematics operation

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,6 +2,13 @@ import { brainGame } from '..';
 
 const rules = 'What is the result of the expression?';
 
+const mathOperations = new Map([
+  [1, '+'],
+  [2, '-'],
+  [3, '*'],
+  [4, '/'],
+]);
+
 const calcGame = () => {
   brainGame(rules);
 };
