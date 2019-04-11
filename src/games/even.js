2408fc3b289eb6d02908ad44bb607fa1cc31512d
George Meshcherov <meshcherov.g@gmail.com>
2019-04-11 15:52:53 +0500

    add import hexlet-pair

--- src/games/even.js
+++ src/games/even.js
@@ -1,8 +1,11 @@
-import { brainGame } from '..';
+import { brainGame, getRandom } from '..';
+import {
+  cons, car, cdr, toString
+} from 'hexlet-pairs';
 
 const rules = 'Answer "yes" if number even otherwise answer "no".';
 
-const conditions = [1, 100];
+const conditions = cons(1, 100);
 
 const getCorrectAnswer = number => (number % 2 ? 'NO' : 'YES');
 
