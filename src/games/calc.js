d8dca020d4c4d8e1cde43f09acff918b2282a1fe
George Meshcherov <meshcherov.g@gmail.com>
2019-04-11 15:53:26 +0500

    add import hexlet pair and test function

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,6 +1,11 @@
 import { brainGame } from '..';
+import {
+  cons, car, cdr, toString
+} from 'hexlet-pairs';
 
 const rules = 'What is the result of the expression?';
+
+const conditions = cons(cons(cons(1, 100), cons(1, 100)), cons(1, 3));
 
 const mathOperations = new Map([
   [1, '+'],
@@ -9,8 +14,22 @@ const mathOperations = new Map([
   [4, '/'],
 ]);
 
+const question = toString();
+
+const getMathOperation = mathOperations.get(car(conditions));
+
+const getNumOne = car(car(conditions));
+
+const getNumTwo = cdr(car(conditions));
+
+const getCorrectAnswer = (a, b, c) => {
+  a = getNumOne;
+  b = getNumTwo;
+  c = getMathOperation;
+};
+
 const calcGame = () => {
-  brainGame(rules);
+  brainGame(rules, conditions, getCorrectAnswer);
 };
 
 export default calcGame;
