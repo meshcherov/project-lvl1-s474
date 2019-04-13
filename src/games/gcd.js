dcbe5b6d6dd510fc88658f7338d866abbdcc0cf9
George Meshcherov <meshcherov.g@gmail.com>
2019-04-13 11:59:18 +0500

    refactoring gcdGame transfer getCorrectAnswer

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,6 +2,29 @@ import brainGame from '..';
 import getRandom from '../utils';
 import { cons } from 'hexlet-pairs';
 
+const getCorrectAnswer = (numOne, numTwo) => {
+  if (numOne === numTwo) {
+    return numOne;
+  }
+  if (numOne > numTwo) {
+    if (numOne % numTwo === 0) {
+      return numTwo;
+    }
+  }
+  if (numOne < numTwo) {
+    if (numTwo % numOne === 0) {
+      return numOne;
+    }
+  }
+  let gcdNum = 0;
+  for (let i = 1; i < numOne && i < numTwo; i += 1) {
+    if ((numOne % i === 0) && (numTwo % i === 0)) {
+      gcdNum = i;
+    }
+  }
+  return gcdNum;
+};
+
 const gcdGame = () => {
   const description = 'Find the greatest common divisor of given numbers.';
 
@@ -9,30 +32,8 @@ const gcdGame = () => {
 
   const numTwo = getRandom(1, 25);
 
-  const getCorrectAnswer = () => {
-    if (numOne === numTwo) {
-      return numOne;
-    }
-    if (numOne > numTwo) {
-      if (numOne % numTwo === 0) {
-        return numTwo;
-      }
-    }
-    if (numOne < numTwo) {
-      if (numTwo % numOne === 0) {
-        return numOne;
-      }
-    }
-    let gcdNum = 0;
-    for (let i = 1; i < numOne && i < numTwo; i += 1) {
-      if ((numOne % i === 0) && (numTwo % i === 0)) {
-        gcdNum = i;
-      }
-    }
-    return gcdNum;
-  };
 
-  const correctAnswer = getCorrectAnswer();
+  const correctAnswer = getCorrectAnswer(numOne, numTwo);
 
   const question = `${numOne} ${numTwo}`;
 
