031da3517ac7436b7d02dd98610e01a71b677bfa
George Meshcherov <meshcherov.g@gmail.com>
2019-04-14 10:28:46 +0500

    refactoring getGreatestCommonDivider

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,33 +4,13 @@ import { cons } from 'hexlet-pairs';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getGreatestCommonDivider = (numOne, numTwo) => {
-  if (numOne === numTwo) {
-    return numOne;
-  }
-  if (numOne > numTwo) {
-    if (numOne % numTwo === 0) {
-      return numTwo;
-    }
-  }
-  if (numOne < numTwo) {
-    if (numTwo % numOne === 0) {
-      return numOne;
-    }
-  }
-  let gcdNum = 0;
-  for (let i = 1; i < numOne && i < numTwo; i += 1) {
-    if ((numOne % i === 0) && (numTwo % i === 0)) {
-      gcdNum = i;
-    }
-  }
-  return gcdNum;
-};
+const getGreatestCommonDivider = (numOne, numTwo) => (numOne !== 0
+  ? getGreatestCommonDivider(numTwo % numOne, numOne) : numTwo);
 
 const gcdGame = () => {
   const numOne = getRandom(1, 25);
   const numTwo = getRandom(1, 25);
-  const correctAnswer = getGreatestCommonDivider(numOne, numTwo);
+  const correctAnswer = (getGreatestCommonDivider(numOne, numTwo)).toString();
 
   const question = `${numOne} ${numTwo}`;
 
